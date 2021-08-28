b775e0347303ede2d9f370c476fa485cbb8eeae5
Kenneth Howe <kcmasterpiece347@gmail.com>
2021-08-28 13:20:36 -0400

    Added torus example.

--- betti_test.py
+++ betti_test.py
@@ -34,6 +34,7 @@ ball.add_boundary_matrix(3, np.array([
     [1]
 ]))
 
+# Expected: [1,0,0,0]
 print(f'3-Ball: {ball.betti_numbers()}')
 
 '''
@@ -68,4 +69,39 @@ klein.add_boundary_matrix(2, np.array([
     [0,0,0,0,0,0,1,1]
 ]))
 
+# Expected: [1,2,1]
 print(f'Klein Bottle: {klein.betti_numbers()}')
+
+'''
+Compute the Betti numbers of the 2-dimensional torus
+'''
+
+torus = SimplicialComplex()
+
+torus.add_boundary_matrix(0, np.array([
+    [1,1,1,1]
+]))
+
+torus.add_boundary_matrix(1, np.array([
+    [1,1,1,0,0,1,1,1,0,0,0,0],
+    [1,1,0,1,1,0,0,0,1,1,0,0],
+    [0,0,1,1,0,0,1,0,0,1,1,1],
+    [0,0,0,0,1,1,0,1,1,0,1,1]
+]))
+
+torus.add_boundary_matrix(2, np.array([
+    [1,0,0,0,0,1,0,0],
+    [0,0,1,0,0,0,0,1],
+    [1,0,0,1,0,0,0,0],
+    [1,1,0,0,0,0,0,0],
+    [0,1,1,0,0,0,0,0],
+    [0,0,1,1,0,0,0,0],
+    [0,0,0,0,1,0,0,1],
+    [0,0,0,0,1,1,0,0],
+    [0,0,0,0,0,1,1,0],
+    [0,0,0,0,0,0,1,1],
+    [0,1,0,0,1,0,0,0],
+    [0,0,0,1,0,0,1,0]
+]))
+
+print(f'Torus: {torus.betti_numbers()}')
