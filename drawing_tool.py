a0d7c5072bbfdc15467494c8ffdf6976c6039fe9
Kenneth Howe <kcmasterpiece347@gmail.com>
2021-09-04 13:21:41 -0400

    Minor variable renaming.

--- drawing_tool.py
+++ drawing_tool.py
@@ -12,7 +12,7 @@ SCREEN = pygame.display.set_mode(SIZE)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 GREY = (114, 114, 114)
-DARK = (178,205,245)
+BLUE = (178,205,245)
 AQUA = (200, 247, 230)
 PINK = 	(245, 178, 205)
 
@@ -88,7 +88,7 @@ def main():
             pygame.draw.line(SCREEN, GREY, e[0], e[1], 2)
         for v in simplex_verts:
             circle = pygame.draw.circle(SCREEN, BLACK, v, 8)
-            pygame.draw.circle(SCREEN, DARK, v, 6)
+            pygame.draw.circle(SCREEN, BLUE, v, 6)
             points.append(circle)
         for s in selected:
             circle = pygame.draw.circle(SCREEN, PINK, s, 6)
